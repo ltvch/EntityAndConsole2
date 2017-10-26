59f2a36c45f07c7c993557900111be7cd634040b
ltvch <HrenHrenov@myrambler.ru>
2017-10-26 20:04:07 +0300

    Добавьте файлы проекта.

---
+++ EntityAndConsole2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityAndConsole2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
